c597893d94643cb72782f91bf3c4a63d1880a45e
Tal Mendelson <tal.mendelson@bluesnap.com>
2025-06-20 12:06:21 +0300

    changed 1

--- Terraform/20_06_25/1/output.tf
+++ Terraform/20_06_25/1/output.tf
@@ -1,7 +1,19 @@
-output "Int_IP_address" { 
-    value = "${docker_container.my_container.network_data[0].ip_address}" 
-} 
+output "internal_ip" {
+  value = docker_container.my_container.ip_address
+}
 
-output "Ext_IP_address" { 
-    value = "${docker_container.my_container.ports[0].ip}" 
+output "internal_port" {
+  value = docker_container.my_container.ports[0].internal
+}
+
+output "external_ip" {
+  value = "localhost" # or your host IP if not using localhost
+}
+
+output "external_port" {
+  value = docker_container.my_container.ports[0].external
+}
+
+output "full_access_url" {
+  value = "http://localhost:${docker_container.my_container.ports[0].external}"
 }
--- Terraform/20_06_25/1/variables.tf
+++ Terraform/20_06_25/1/variables.tf
@@ -1,32 +1,30 @@
-variable "env" {
-    description = "env: dev or prod"
-}
+variable "env" { 
+description = "env: dev or prod" 
+} 
 
-variable "image_name" {
-    type = "map"
-    description = "name of the image that will be used"
-    default = {
-        dev  = "ghost:latest"
-        prod = "ghost:alpine" 
-    }
-}
-
+variable "image_name" { 
+type = "map" 
+description = "name of the image that will be used" 
+default = { 
+    dev = "ghost:latest" 
+    prod = "ghost:alpine" 
+} 
+} 
 variable "container_name" { 
-    type = "map"
-    default = {
-        dev = "blog_dev"
-        prod = "blog_prod"
-        }
-}
-
+type = "map" 
+default = { 
+    dev = "blog_dev" 
+    prod = "blog_prod" 
+} 
+} 
 variable "int_port" { 
-    default = "2368" 
+default = "2368" 
 } 
 
 variable "ext_port" { 
-    type = "map"
-    default = {
-        dev = "8081"
-        prod = "8080"
-    }
+type = "map" 
+default = { 
+    dev = "8081" 
+    prod = "8080" 
+} 
 }
