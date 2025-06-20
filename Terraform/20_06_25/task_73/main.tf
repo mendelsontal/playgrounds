54d2355a09c55e7f820b9c93012047e07f136260
Tal Mendelson <tal.mendelson@bluesnap.com>
2025-06-20 12:32:55 +0300

    task 73

---
+++ Terraform/20_06_25/task_73/main.tf
@@ -0,0 +1,17 @@
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+}
+
+resource "docker_container" "my_nginx_container" {
+    name  = "nginx_task73"
+    image = docker_image.nginx.image_id
+    ports {
+        internal:80
+        external = 80
+    }
+
+    volumes {
+        host_path      = "${path.module}/index.html"
+        container_path = "/usr/share/nginx/html/index.html"
+    }
+}
