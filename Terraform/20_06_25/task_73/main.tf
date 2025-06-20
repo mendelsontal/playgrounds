22e435e02f7dbad59b0b98ad3dd5e3f7aed65a91
Tal Mendelson <tal.mendelson@bluesnap.com>
2025-06-20 12:43:21 +0300

    fix

--- Terraform/20_06_25/task_73/main.tf
+++ Terraform/20_06_25/task_73/main.tf
@@ -1,17 +1,17 @@
 resource "docker_image" "nginx" {
-    name = "nginx:latest"
+  name = "nginx:latest"
 }
 
-resource "docker_container" "my_nginx_container" {
-    name  = "nginx_task73"
-    image = docker_image.nginx.image_id
-    ports {
-        internal = 80
-        external = 80
-    }
+resource "docker_container" "nginx_container" {
+  name  = "my-nginx"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 80
+  }
 
     volumes {
-        host_path      = "${path.module}/index.html"
+        host_path      = abspath("${path.module}/index.html")
         container_path = "/usr/share/nginx/html/index.html"
     }
 }
